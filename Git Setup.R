4182cc6be559c40e014a4ab592b79cbbef9a9ab0
marcoheimanntrading <marcoheimanntrading@gmail.com>
2021-04-30 12:15:05 +0200

    First steps with Github

---
+++ Git Setup.R
@@ -0,0 +1,8 @@
+https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
+
+Make sure to copy your new personal access token now. You won’t be able to see it again!
+  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
+
+library(gitcreds)
+gitcreds_set()
+3
